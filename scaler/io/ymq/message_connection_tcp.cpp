sharpener6/scaler

commit 7fe0a918ac4197f5881f098477a2f8ebdcdfd784

Fix C++ Warnings (#358)
* Fix C++ Warnings

* remove generic

* Replace using and tuple with struct

scaler/io/ymq/timed_queue.h
@@ -19,6 +19,14 @@
 namespace scaler {
 namespace ymq {
 
+struct TimedCallback {
+    Timestamp timestamp;
+    Configuration::TimedQueueCallback callback;
+    Configuration::ExecutionCancellationIdentifier identifier;
+
+    constexpr bool operator<(const TimedCallback& other) const { return timestamp < other.timestamp; }
+};
+
 #ifdef __linux__
 inline int createTimerfd()
 {
@@ -61,12 +69,6 @@ inline int createTimerfd()
 
 class TimedQueue {
 public:
-    using Callback            = Configuration::TimedQueueCallback;
-    using Identifier          = Configuration::ExecutionCancellationIdentifier;
-    using TimedFunc           = std::tuple<Timestamp, Callback, Identifier>;
-    constexpr static auto cmp = [](const auto& x, const auto& y) { return std::get<0>(x) < std::get<0>(y); };
-    using PriorityQueue       = std::priority_queue<TimedFunc, std::vector<TimedFunc>, decltype(cmp)>;
-
     TimedQueue(): _timerFd(createTimerfd()), _currentId {} { assert(_timerFd); }
     ~TimedQueue()
     {
@@ -74,10 +76,10 @@ public:
             close(_timerFd);
     }
 
-    Identifier push(Timestamp timestamp, Callback cb)
+    Configuration::ExecutionCancellationIdentifier push(Timestamp timestamp, Configuration::TimedQueueCallback cb)
     {
         auto ts = convertToItimerspec(timestamp);
-        if (pq.empty() || timestamp < std::get<0>(pq.top())) {
+        if (pq.empty() || timestamp < pq.top().timestamp) {
             int ret = timerfd_settime(_timerFd, 0, &ts, nullptr);
             if (ret == -1) {
                 unrecoverableError({
@@ -93,9 +95,9 @@ public:
         return _currentId++;
     }
 
-    void cancelExecution(Identifier id) { _cancelledFunctions.insert(id); }
+    void cancelExecution(Configuration::ExecutionCancellationIdentifier id) { _cancelledFunctions.insert(id); }
 
-    std::vector<Callback> dequeue()
+    std::vector<Configuration::TimedQueueCallback> dequeue()
     {
         uint64_t numItems;
         ssize_t n = read(_timerFd, &numItems, sizeof numItems);
@@ -110,12 +112,12 @@ public:
             });
         }
 
-        std::vector<Callback> callbacks;
+        std::vector<Configuration::TimedQueueCallback> callbacks;
 
         Timestamp now;
         while (pq.size()) {
-            if (std::get<0>(pq.top()) < now) {
-                auto [ts, cb, id] = std::move(const_cast<PriorityQueue::reference>(pq.top()));
+            if (pq.top().timestamp < now) {
+                auto [ts, cb, id] = std::move(const_cast<std::priority_queue<TimedCallback>::reference>(pq.top()));
                 pq.pop();
                 auto cancelled = _cancelledFunctions.find(id);
                 if (cancelled != _cancelledFunctions.end()) {
@@ -128,7 +130,7 @@ public:
         }
 
         if (!pq.empty()) {
-            auto nextTs = std::get<0>(pq.top());
+            auto nextTs = pq.top().timestamp;
             auto ts     = convertToItimerspec(nextTs);
             int ret     = timerfd_settime(_timerFd, 0, &ts, nullptr);
             if (ret == -1) {
@@ -171,9 +173,9 @@ public:
 
 private:
     int _timerFd;
-    Identifier _currentId;
-    PriorityQueue pq;
-    std::set<Identifier> _cancelledFunctions;
+    Configuration::ExecutionCancellationIdentifier _currentId;
+    std::priority_queue<TimedCallback> pq;
+    std::set<Configuration::ExecutionCancellationIdentifier> _cancelledFunctions;
 };
 
 #endif  // __linux__
@@ -181,11 +183,6 @@ private:
 #ifdef _WIN32
 class TimedQueue {
 public:
-    using Callback            = Configuration::TimedQueueCallback;
-    using Identifier          = Configuration::ExecutionCancellationIdentifier;
-    using TimedFunc           = std::tuple<Timestamp, Callback, Identifier>;
-    constexpr static auto cmp = [](const auto& x, const auto& y) { return std::get<0>(x) < std::get<0>(y); };
-    using PriorityQueue       = std::priority_queue<TimedFunc, std::vector<TimedFunc>, decltype(cmp)>;
     HANDLE _completionPort;
     const size_t _key;
 

scaler/io/ymq/pymod_ymq/gil.h
@@ -2,13 +2,13 @@
 
 class AcquireGIL {
 public:
-    AcquireGIL() : _state(PyGILState_Ensure()) {}
+    AcquireGIL(): _state(PyGILState_Ensure()) {}
     ~AcquireGIL() { PyGILState_Release(_state); }
 
-    AcquireGIL(const AcquireGIL&) = delete;
+    AcquireGIL(const AcquireGIL&)            = delete;
     AcquireGIL& operator=(const AcquireGIL&) = delete;
-    AcquireGIL(AcquireGIL&&) = delete;
-    AcquireGIL& operator=(AcquireGIL&&) = delete;
+    AcquireGIL(AcquireGIL&&)                 = delete;
+    AcquireGIL& operator=(AcquireGIL&&)      = delete;
 
 private:
     PyGILState_STATE _state;

commit 660679e9b8ac4714a1fc7260aca8180f68099698

Fix Errors on Windows introduced by #358 (#375)
* Add test preset for Visual Studio to detect test cases

Signed-off-by: gxu <georgexu420@163.com>

* Fix Build Errors on Windows

Signed-off-by: gxu <georgexu420@163.com>

* Improve add_test_executable for Windows

Signed-off-by: gxu <georgexu420@163.com>

* Remove redundant test directory

Signed-off-by: gxu <georgexu420@163.com>

* Migrate and fixup logging test for Windows

Signed-off-by: gxu <georgexu420@163.com>

* Tmp fix bad logic on Windows

Signed-off-by: gxu <georgexu420@163.com>

* revert not build test_ymq on Windows

Signed-off-by: gxu <georgexu420@163.com>

* Bump Version Number

Signed-off-by: gxu <georgexu420@163.com>

* Remove dynamic link flag

Signed-off-by: gxu <georgexu420@163.com>

---------

Signed-off-by: gxu <georgexu420@163.com>
Co-authored-by: sharpener6 <1sc2l4qi@duck.com>

scaler/io/ymq/message_connection_tcp.cpp
@@ -121,6 +121,7 @@ void MessageConnectionTCP::onCreated()
         if (lastError == ERROR_IO_PENDING) {
             return;
         }
+
         unrecoverableError({
             Error::ErrorCode::CoreBug,
             "Originated from",
@@ -352,6 +353,10 @@ void MessageConnectionTCP::onRead()
     }
 
 #ifdef _WIN32
+    // TODO: This need rewrite to better logic
+    if (!_connFd) {
+        return;
+    }
     const bool ok = ReadFile((HANDLE)(SOCKET)_connFd, nullptr, 0, nullptr, this->_eventManager.get());
     if (ok) {
         onRead();

scaler/io/ymq/timed_queue.h
@@ -199,10 +199,10 @@ public:
         }
     }
 
-    Identifier push(Timestamp timestamp, Callback cb)
+    Configuration::ExecutionCancellationIdentifier push(Timestamp timestamp, Configuration::TimedQueueCallback cb)
     {
         auto ts = convertToLARGE_INTEGER(timestamp);
-        if (pq.empty() || timestamp < std::get<0>(pq.top())) {
+        if (pq.empty() || timestamp < pq.top().timestamp) {
             SetWaitableTimer(
                 _timerFd,
                 (LARGE_INTEGER*)&ts,
@@ -218,16 +218,16 @@ public:
         return _currentId++;
     }
 
-    void cancelExecution(Identifier id) { _cancelledFunctions.insert(id); }
+    void cancelExecution(Configuration::ExecutionCancellationIdentifier id) { _cancelledFunctions.insert(id); }
 
-    std::vector<Callback> dequeue()
+    std::vector<Configuration::TimedQueueCallback> dequeue()
     {
-        std::vector<Callback> callbacks;
+        std::vector<Configuration::TimedQueueCallback> callbacks;
 
         Timestamp now;
         while (pq.size()) {
-            if (std::get<0>(pq.top()) < now) {
-                auto [ts, cb, id] = std::move(const_cast<PriorityQueue::reference>(pq.top()));
+            if (pq.top().timestamp < now) {
+                auto [ts, cb, id] = std::move(const_cast<std::priority_queue<TimedCallback>::reference>(pq.top()));
                 pq.pop();
                 auto cancelled = _cancelledFunctions.find(id);
                 if (cancelled != _cancelledFunctions.end()) {
@@ -240,7 +240,7 @@ public:
         }
 
         if (!pq.empty()) {
-            auto nextTs = std::get<0>(pq.top());
+            auto nextTs = pq.top().timestamp;
             auto ts     = convertToLARGE_INTEGER(nextTs);
             SetWaitableTimer(
                 _timerFd,
@@ -258,9 +258,9 @@ public:
 
 private:
     HANDLE _timerFd;
-    Identifier _currentId;
-    PriorityQueue pq;
-    std::set<Identifier> _cancelledFunctions;
+    Configuration::ExecutionCancellationIdentifier _currentId;
+    std::priority_queue<TimedCallback> pq;
+    std::set<Configuration::ExecutionCancellationIdentifier> _cancelledFunctions;
 };
 
 #endif  // _WIN32

tests/CMakeLists.txt
@@ -20,11 +20,9 @@ function(add_test_executable test_name source_file)
 
     target_include_directories(${test_name} PRIVATE ${CMAKE_BINARY_DIR})
 
-    target_link_libraries(${test_name} PRIVATE 
-        $<$<CXX_COMPILER_ID:GNU,Clang>:-Wl,-Bstatic>
-        object_storage_server_objs
+    target_link_libraries(${test_name} PRIVATE
+        $<$<NOT:$<PLATFORM_ID:Windows>>:object_storage_server_objs>
         ymq_objs
-        $<$<CXX_COMPILER_ID:GNU,Clang>:-Wl,-Bdynamic>
         CapnProto::capnp
         CapnProto::kj
         GTest::gtest_main
@@ -32,12 +30,19 @@ function(add_test_executable test_name source_file)
     )
 
     add_test(NAME ${test_name} COMMAND ${test_name})
+
+    # Post build: We need to copy if dll is not present, otherwise the test program won't find gtest.dll
+    # See https://stackoverflow.com/questions/69978314/cmake-with-gtest-on-windows-build-starts-test-but-shared-libs-cannot-be-found
+    if (WIN32)
+        add_custom_command(TARGET ${test_name} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                $<TARGET_RUNTIME_DLLS:${test_name}>
+                $<TARGET_FILE_DIR:${test_name}>
+            COMMAND_EXPAND_LISTS
+        )
+    endif()
 endfunction()
 
-if(LINUX OR APPLE)
-  # Add the directory for the C++ tests.
-  add_subdirectory(cpp)
+# Add the directory for the C++ tests.
+add_subdirectory(cpp)
 
-  # Add the new directory for io tests.
-  add_subdirectory(io/ymq)
-endif()

tests/cpp/CMakeLists.txt
@@ -1,3 +1,6 @@
 # this fetches Google Test, so it must be included first.
-add_subdirectory(object_storage)
+if(LINUX OR APPLE)
+	add_subdirectory(object_storage)
+endif()
+
 add_subdirectory(ymq)

tests/cpp/ymq/CMakeLists.txt
@@ -1,1 +1,3 @@
 add_test_executable(test_ymq test_ymq.cpp)
+
+add_test_executable(test_ymq_logging test_ymq_logging.cpp)

tests/cpp/ymq/test_ymq_logging.cpp
@@ -10,6 +10,10 @@
 
 #include "scaler/io/ymq/logging.h"
 
+#ifdef max
+#undef max
+#endif
+
 // Test fixture for direct unit testing of the log() function's formatting
 class LoggingUnitTest: public ::testing::Test {
 protected:

tests/io/ymq/CMakeLists.txt
@@ -1,1 +1,0 @@
-add_test_executable(test_logging test_logging.cpp)